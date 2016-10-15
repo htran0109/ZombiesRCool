f48db59466ccaca658cdd42016448cc727e8290c
Hung Tran <hqtran@ucsd.edu>
2016-10-14 21:03:47 -0700

    Platforming code simplified

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class Character : MonoBehaviour {
 
 
-    public float maxWalkingSpeed = .2f;
-    public float jumpSpeed = .3f;
+    public float walkForce= 50f;
+    public float jumpForce = 200f;
     private int jumpingCount = 0;
-    public const int MAX_JUMP_COUNT = 5;
+    public const int MAX_JUMP_COUNT = 6;
     private bool holstered = true;
     private bool grounded = false;
     private Vector3 speed;
+    Rigidbody2D thisBody;
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector2(-3, -3);
         holstered = true;
         speed = new Vector3(0, 0);
+        thisBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         readKeys();
-        readMovement();
         Debug.Log(speed.y);
 
 	}
@@ -30,73 +31,21 @@ public class Character : MonoBehaviour {
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            if(speed.x >= -(maxWalkingSpeed))
-            {
-                speed.x -= .03f;
-            }
+            thisBody.AddForce(new Vector2(-walkForce, 0));
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (speed.x <= maxWalkingSpeed)
-            {
-                speed.x += .03f;
-            }
+            thisBody.AddForce(new Vector2(walkForce, thisBody.velocity.y));
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
             
+            thisBody.AddForce(new Vector2(0, jumpForce));
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow) && grounded)
-        {
-            if(holstered)
-            {
-                jumpingCount = MAX_JUMP_COUNT;
-                grounded = false;
-            }
-        }
-        transform.Translate(speed);
-        
-        
-    }
 
-    void readMovement()
-    {
-        if(speed.x > 0)
-        {
-            speed.x -= .01f;
-        }
-        else if(speed.x < 0)
-        {
-            speed.x += .01f;
-        }
-        if(Mathf.Abs(speed.x)<.005f)
-        {
-            speed.x = 0f;
-        }
-        if (jumpingCount > 0)
-        {
-            jumpingCount--;
-            speed.y = jumpSpeed;
-        }
-        else
-        {
-            if (speed.y > 0)
-            {
-                speed.y -= .01f;
-            }
-            else if (speed.y < 0)
-            {
-                speed.y += .01f;
-            }
-            if (Mathf.Abs(speed.y) < .005f)
-            {
-                speed.y = 0f;
-            }
-        }
+        
+        
     }
     
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if(coll.gameObject.name.Contains("Ground"))
-        {
-            grounded = true;
-        }
-    }
+    
 }
