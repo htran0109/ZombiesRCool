7655342edf6fe850100ccf2b2675d0d655da4474
Hung Tran <hqtran@ucsd.edu>
2016-10-14 21:43:13 -0700

    Platforming code actually works

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -3,48 +3,84 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
-
+    public const float MAX_WALK_SPEED = 3f;
+    public const float SLOW_FORCE = 20f;
     public float walkForce= 50f;
-    public float jumpForce = 200f;
-    private int jumpingCount = 0;
-    public const int MAX_JUMP_COUNT = 6;
+    public float jumpForce = 1000f;
     private bool holstered = true;
     private bool grounded = false;
-    private Vector3 speed;
+    private bool moving = false;
     Rigidbody2D thisBody;
+    RaycastHit2D ground;
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector2(-3, -3);
         holstered = true;
-        speed = new Vector3(0, 0);
         thisBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         readKeys();
-        Debug.Log(speed.y);
-
-	}
+        slowWalk();
+  	}
 
     void readKeys()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            thisBody.AddForce(new Vector2(-walkForce, 0));
+            moving = true;
+            if (thisBody.velocity.x > -MAX_WALK_SPEED)
+            {
+                thisBody.AddForce(new Vector2(-walkForce, 0));
+            }
+            else
+            {
+                thisBody.velocity = new Vector2(-MAX_WALK_SPEED, thisBody.velocity.y);
+            }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            thisBody.AddForce(new Vector2(walkForce, thisBody.velocity.y));
+            moving = true;
+            if (thisBody.velocity.x < MAX_WALK_SPEED)
+            {
+                thisBody.AddForce(new Vector2(walkForce, thisBody.velocity.y));
+            }
+            else
+            {
+                thisBody.velocity = new Vector2(MAX_WALK_SPEED, thisBody.velocity.y);
+            }
         }
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
-            thisBody.AddForce(new Vector2(0, jumpForce));
+
+            ground = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+            if (ground.collider.gameObject.name.Contains("Ground"))
+            {
+                Debug.Log(ground.distance);
+                thisBody.AddForce(new Vector2(0, jumpForce));
+
+            }
         }
+        moving = false;
 
         
         
+    }
+
+    void slowWalk()
+    {
+        if (!moving)
+        {
+            if (thisBody.velocity.x > 0)
+            {
+                thisBody.AddForce(new Vector2(-SLOW_FORCE, 0));
+            }
+            else if (thisBody.velocity.x < 0)
+            {
+                thisBody.AddForce(new Vector2(SLOW_FORCE, 0));
+            }
+        }
     }
     
     
